a0a0d4fb73daa656e04d36b8153233662996767d
pentolcode88 <137736813+pentolcode88@users.noreply.github.com>
2023-07-12 19:23:18 -0400

    Update and rename cc_cvc.php to conf.php

---
+++ conf.php
@@ -0,0 +1,37 @@
+<?php
+  define('HTTP_SERVER', 'http://www.alps-printer.com/');
+  define('HTTPS_SERVER', 'https://www.alps-printer.com/');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', '.alps-printer.com');
+  define('HTTPS_COOKIE_DOMAIN', '.alps-printer.com');
+  define('HTTP_COOKIE_PATH', '/'); 
+  define('HTTPS_COOKIE_PATH', '/');
+  define('DIR_WS_HTTP_CATALOG', '');
+  define('DIR_WS_HTTPS_CATALOG', '');
+  define('DIR_WS_IMAGES', 'images/');
+  define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
+  define('DIR_WS_BANNERS', DIR_WS_IMAGES . 'banners/');
+  define('DIR_WS_INCLUDES', 'includes/');
+  define('DIR_WS_BOXES', DIR_WS_INCLUDES . 'boxes/');
+  define('DIR_WS_FUNCTIONS', DIR_WS_INCLUDES . 'functions/');
+  define('DIR_WS_CLASSES', DIR_WS_INCLUDES . 'classes/');
+  define('DIR_WS_MODULES', DIR_WS_INCLUDES . 'modules/');
+  define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
+
+  define('DIR_WS_DOWNLOAD_PUBLIC', 'pub/');
+  define('DIR_FS_CATALOG', '/www/web/alpsprinter/public_html/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'root');
+  define('DB_SERVER_PASSWORD', '8VzWTnr#472');
+  define('DB_DATABASE', 'alps_printer');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+  define('STORE_OWNER_EMAIL_ADDRESS', 'nafisfadhil873@gmail.com');
+  define('EMAIL_FROM', 'nafisfadhil873@gmail.com');
+  define('SEND_EXTRA_ORDER_EMAILS_TO', 'nafisfadhil873@gmail.com');
+  define('MODULE_PAYMENT_PAYPAL_IPN_ID', 'nafisfadhil873@gmail.com');
+  define('MODULE_PAYMENT_PAYPAL_IPN_DEBUG_EMAIL', 'nafisfadhil873@gmail.com');
+?>
